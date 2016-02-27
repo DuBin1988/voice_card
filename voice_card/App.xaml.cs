ae1bba9deb78f9633c234472142835a3ff8ccc93
bianzk <61577962@>
2016-02-27 12:15:24 +0800

    初始导入

---
+++ voice_card/App.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using log4net.Config;
+using System.IO;
+using voice_card.helper;
+using System.Data.OleDb;
+using voice_card.db;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Diagnostics;
+
+namespace voice_card
+{
+    /// <summary>
+    /// 
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        Window1 window1;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Process thisProc = Process.GetCurrentProcess();
+            // Check how many total processes have the same name as the current one
+            if (Process.GetProcessesByName(thisProc.ProcessName).Length > 1)
+            {
+                // If ther is more than one, than it is already running.
+                MessageBox.Show("请不要重复运行本程序，服务系统可以切换用户查看是否在运行！");
+                Application.Current.Shutdown();
+                return;
+            }else{
+                base.OnStartup(e);
+                log4net.Config.XmlConfigurator.Configure(new FileInfo("log4net.config"));
+                String currentDir = Environment.CurrentDirectory;
+                XmlService.load(currentDir+"\\config.xml");
+                DBHelper.DBFactoryInit();
+
+                Random r = new Random();
+                for (int i = 0; i < 10; i++)
+                {
+                    int u = r.Next(5);
+                    Console.WriteLine(u);
+                }
+
+                window1 = new Window1();
+                if (e.Args.Length > 0)
+                {
+                    window1.ShowEx(window1,e.Args[0]);
+                }else{ 
+                    window1.Show();
+                }
+            }
+
+            
+        }
+
+    }
+}
---
+++ voice_card/Window1.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using voice_card.service;
+using System.Collections;
+using voice_card.entity;
+using System.Windows.Threading;
+using System.Collections.ObjectModel;
+using voice_card.helper;
+
+namespace voice_card
+{
+    public partial class Window1 : Window
+    {
+        WorkService workService;
+        ObservableCollection<LineInfo> obColl = new ObservableCollection<LineInfo>();
+        System.Windows.RoutedEventArgs ee = new System.Windows.RoutedEventArgs();
+
+        public ObservableCollection<LineInfo> ObColl
+        { get { return obColl; } }
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            if (workService != null)
+            {
+                workService.Exit();
+                workService.CloseServer();
+            }
+            this.Close();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.button1.IsEnabled = false;
+            this.button1.Content = "已运行";
+            workService = new WorkService();
+            workService.LoadDriver(obColl);
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(timer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0,0,0,0,100);
+            dispatcherTimer.Start();
+            //web启动
+            workService.StartServer();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            workService.Run();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        internal void ShowEx(Window1 window1,string p)
+        {
+           // throw new NotImplementedException();
+            if(null !=p && "-run".Equals(p)){
+                window1.Show();
+                window1.button1_Click(button1,ee);
+            }
+
+        }
+    }
+}
